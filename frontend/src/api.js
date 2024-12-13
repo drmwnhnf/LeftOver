2c6514505e344a7b928cbf77bf3cc7359de4e2e0
drmwnhnf <darmawanhaneef@gmail.com>
2024-12-13 07:50:10 +0700

    Refactored Branch Name

---
+++ frontend/src/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:2393/api"; // Update to your backend URL
+
+// Login
+export const login = async (credentials) => {
+  return axios.post(`${API_URL}/account/login`, credentials);
+};
+
+// Register
+export const register = async (userData) => {
+  return axios.post(`${API_URL}/account/register`, userData);
+};
+
+// Fetch all items (Example for "Read")
+export const fetchItems = async () => {
+  return axios.get(`${API_URL}/items`);
+};
+
+// Create item
+export const createItem = async (data) => {
+  return axios.post(`${API_URL}/items`, data);
+};
+
+// Update item
+export const updateItem = async (id, data) => {
+  return axios.put(`${API_URL}/items/${id}`, data);
+};
+
+// Delete item
+export const deleteItem = async (id) => {
+  return axios.delete(`${API_URL}/items/${id}`);
+};
